96c836ab3da77bb0e79386c3384cf3e6d8cb2d6b
Alberto Velázquez Rapado <86166779+AlVelRap@users.noreply.github.com>
2023-06-25 21:30:16 +0200

    commit

--- front-dietario/src/services/http-comun.ts
+++ front-dietario/src/services/http-comun.ts
@@ -3,10 +3,10 @@ import type { AxiosInstance } from "axios";
 import { VERSION, REST, API } from "../util/constants";
 
 // URL Para desarollo
-const url_backend = `http://localhost:3000/${API}/${REST}/${VERSION}`;
+// const url_backend = `http://localhost:3000/${API}/${REST}/${VERSION}`;
 
 // Recuerda! Aqui va la IP!! Recuerda cambiarla al mover el portatil
-// const url_backend =  `http://192.168.1.142:6868/${API}/${REST}/${VERSION}`
+const url_backend =  `http://192.168.1.142:6868/${API}/${REST}/${VERSION}`
 
 const apiClient: AxiosInstance = axios.create({
   baseURL: url_backend,
